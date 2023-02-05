9b1729f9805a86386a702f3e240726cd66d1b199
ksio7 <sergiusparedes@gmail.com>
2023-02-05 23:29:48 +0100

    añadido sergio.java

--- Ejercicio1ED/src/Sergio.java
+++
@@ -1,9 +0,0 @@
-
-public class Sergio {
-
-	public static void main(String[] args) {
-		
-		System.out.println("Hola soy Sergio");
-	}
-
-}
---
+++ Ejercicio1ED/src/Sergio/sergio.java
@@ -0,0 +1,10 @@
+package Sergio;
+
+public class sergio {
+
+	public static void main(String[] args) {
+System.out.println("Hola soy sergio");
+System.out.println("Mis 3 hobbies son: Deportes, leer y el cine");
+	}
+
+}
