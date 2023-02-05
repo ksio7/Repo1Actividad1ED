4944af89d91fdd00bfb29a5bdc54219a80b1dff0
ksio7 <124289885+ksio7@users.noreply.github.com>
2023-02-05 10:09:28 +0100

    Update Sergio.java

--- Ejercicio1ED/Sergio/Sergio.java
+++ Ejercicio1ED/Sergio/Sergio.java
@@ -5,6 +5,8 @@ public class sergio {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola soy Sergio");
+		System.out.println("Mis 3 hobbies, son: Deportes, leer y videojuegos");
+
 
 	}
 
